6350c03364ba0dedf8c1eb6be1046828230115a1
Monica Powell <monica@aboutmonica.com>
2020-01-23 22:33:04 -0500

    initial commit with basic functionality working - generating photos and overlay

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import Select from "react-dropdown-select";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function getBreed(url) {
+  if (url === "") return "dog";
+  const parser = document.createElement("a");
+  parser.href = url;
+  const breed = parser.href.split("/").slice(-2)[0];
+  return breed;
+}
+
+// export function getBreeds() {
+//   return fetch("https://dog.ceo/api/breeds/list/all").then(response =>
+//     response.json().then(json => Object.keys(json.message))
+//   );
+// }
+
+class DogAPICall extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: false, imgUrl: "", breeds: [] };
+
+    this.setState({ loading: true });
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, imgUrl: json.message }));
+  }
+
+  handleClick = api => e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, imgUrl: json.message }));
+  };
+
+  render() {
+    const { imgUrl } = this.state;
+
+    return (
+      <>
+        <button onClick={this.handleClick()} className="button">
+          Say "Hi!" to another dog {""}
+          <span role="img" aria-label="dog-emoji">
+            🐶
+          </span>
+        </button>
+
+        <div className="dog-image">
+          {" "}
+          {imgUrl && <img src={imgUrl} alt={`a ${getBreed(imgUrl)}`} />}
+          <p>
+            <span>W</span>
+            <span>e</span> {""}
+            <span> ❤ </span> {""}
+            <span>o</span>
+            <span>u</span>
+            <span>r</span>
+            {""} {""}
+            <span>p</span>
+            <span>u</span>
+            <span>p</span>
+            <span>s</span>
+            <span>!</span>
+          </p>
+        </div>
+      </>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dogapooloza</h1>
+        <DogAPICall />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App, { getBreed } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("getBreed returns the proper breed when given a URL response from https://images.dog.ceo/breeds", () => {
+  expect(
+    getBreed("https://images.dog.ceo/breeds/keeshond/n02112350_7317.jpg")
+  ).toBe("keeshond");
+});
+test("getBreed returns dog when passed an empty URL", () => {
+  expect(getBreed("")).toBe("dog");
 });
